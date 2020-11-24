61ec30c06f779e3422a7c833d723c4d5f439eedf
Ale <alearanibar@gmail.com>
2020-11-23 23:57:24 -0800

    setup

---
+++ src/components/post/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Post = () => {
+    return (
+        <div>
+            <h3>Username</h3>
+            <img src="https://www.freecodecamp.org/news" alt=""/>
+            <h4>Usernam: caption</h4>
+        </div>
+    )
+}
+
+export default Post
